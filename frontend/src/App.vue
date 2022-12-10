107f205fdce9e3619285a924b663ce6bc28003f8
Lockár Miklós <darkmiclos2002002@gmail.com>
2022-12-10 18:40:33 +0100

    Setup frontend vue project.

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+</style>
